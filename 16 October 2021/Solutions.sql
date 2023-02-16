083505157db7ae8b58c748fe58bd60ba64849c84
Petar Kobakov <pkobakov@gmail.com>
2023-02-16 16:12:56 +0200

    8. First 5 Cigars

--- 16 October 2021/Solutions.sql
+++ 16 October 2021/Solutions.sql
@@ -116,4 +116,18 @@ ON cc.CigarId = cig.Id
 WHERE cig.Id IS NULL
 ORDER BY c.FirstName
 
+-- 8. First 5 Cigars
+
+SELECT TOP(5) c.CigarName, c.PriceForSingleCigar, c.ImageURL 
+FROM Cigars AS c
+JOIN Sizes AS s
+ON c.SizeId = s.Id
+WHERE 
+s.Length >= 12 AND 
+(c.CigarName LIKE '%ci%' OR
+c.PriceForSingleCigar > 50) 
+AND s.RingRange > 2.55
+ORDER BY c.CigarName, 
+c.PriceForSingleCigar DESC
+
 
