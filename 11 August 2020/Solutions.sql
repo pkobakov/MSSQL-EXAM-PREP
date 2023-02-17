836c3ac91b26382c12c95d98da6e633aa34200b7
Petar Kobakov <pkobakov@gmail.com>
2023-02-17 12:13:52 +0200

    1. Database design

---
+++ 11 August 2020/Solutions.sql
@@ -0,0 +1,61 @@
+CREATE DATABASE Bakery
+
+CREATE TABLE Countries
+(
+ Id INT IDENTITY PRIMARY KEY,
+ Name NVARCHAR(50) UNIQUE
+)
+
+CREATE TABLE Customers
+(
+ Id INT IDENTITY PRIMARY KEY,
+ FirstName NVARCHAR(25),
+ LastName NVARCHAR(25),
+ Gender CHAR CHECK(Gender IN('M', 'F')),
+ Age INT,
+ PhoneNumber VARCHAR(10) CHECK(LEN(PhoneNumber) = 10),
+ CountryId INT FOREIGN KEY REFERENCES Countries(Id)
+)
+
+CREATE TABLE Products
+(
+ Id INT IDENTITY PRIMARY KEY,
+ Name NVARCHAR(25) UNIQUE,
+ Description NVARCHAR(250),
+ Recipe NVARCHAR(MAX),
+ Price DECIMAL CHECK(Price>=0)
+)
+
+CREATE TABLE Feedbacks
+(
+ Id INT IDENTITY PRIMARY KEY,
+ Description NVARCHAR(255),
+ Rate DECIMAL (18,2) CHECK(Rate BETWEEN 0 AND 10),
+ ProductId INT FOREIGN KEY REFERENCES Products(Id),
+ CustomerId INT FOREIGN KEY REFERENCES Customers(Id)
+)
+
+CREATE TABLE Distributors
+(
+ Id INT IDENTITY PRIMARY KEY,
+ Name NVARCHAR(25) UNIQUE,
+ AddressText NVARCHAR(30),
+ Summary NVARCHAR(200),
+ CountryId INT FOREIGN KEY REFERENCES Countries(Id)
+)
+
+CREATE TABLE Ingredients
+(
+ Id INT IDENTITY PRIMARY KEY,
+ Name NVARCHAR(30),
+ Description NVARCHAR(200),
+ OriginCountryId INT FOREIGN KEY REFERENCES Countries(Id),
+ DistributorId INT FOREIGN KEY REFERENCES Distributors(Id)
+)
+
+CREATE TABLE ProductsIngredients
+(
+ ProductId INT FOREIGN KEY REFERENCES Products(Id),
+ IngredientId INT FOREIGN KEY REFERENCES Ingredients(Id),
+ PRIMARY KEY (ProductId, IngredientId)
+)
