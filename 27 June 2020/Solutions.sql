2d60838b8774a9b4654e65a7959af1340ee32404
Petar Kobakov <pkobakov@gmail.com>
2023-02-18 12:28:18 +0200

    9. Past Expenses

--- 27 June 2020/Solutions.sql
+++ 27 June 2020/Solutions.sql
@@ -165,3 +165,19 @@ WHERE (
       ) IS NULL OR j.JobId IS NULL
 GROUP BY m.MechanicId, m.FirstName, m.LastName
 ORDER BY m.MechanicId
+
+-- 9. Past Expenses
+
+SELECT j.JobId, SUM(ISNULL(op.Quantity*p.Price,0)) AS Total FROM Jobs AS j
+LEFT JOIN Orders AS o
+ON j.JobId = o.JobId
+LEFT JOIN OrderParts AS op
+ON o.OrderId = op.OrderId
+LEFT JOIN Parts AS p
+ON op.PartId = p.PartId
+WHERE j.Status LIKE 'Finished'
+GROUP BY j.JobId
+ORDER BY Total DESC, j.JobId
+
+
+
