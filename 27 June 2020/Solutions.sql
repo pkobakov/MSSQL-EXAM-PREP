49ef72df8ba8b70ae1dfb81aac93aa5216d88ad5
Petar Kobakov <pkobakov@gmail.com>
2023-02-18 10:24:02 +0200

    06. Current Clients

--- 27 June 2020/Solutions.sql
+++ 27 June 2020/Solutions.sql
@@ -125,3 +125,15 @@ ORDER BY
 m.MechanicId, 
 j.IssueDate,
 j.JobId
+
+-- 6. Current Clients
+
+SELECT 
+CONCAT_WS(' ', c.FirstName, C.LastName) AS Client,
+DATEDIFF(DAY, j.IssueDate, '2017-04-24' ) AS [Days going],
+j.Status
+FROM Clients AS c
+JOIN Jobs AS j
+ON c.ClientId = j.ClientId
+WHERE j.Status <> 'Finished'
+ORDER BY [Days going] DESC, c.ClientId
