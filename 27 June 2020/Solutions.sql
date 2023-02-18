25cc004457f447a58c765cee08bf240f629591f3
Petar Kobakov <pkobakov@gmail.com>
2023-02-18 14:04:20 +0200

    10. Missing Parts

--- 27 June 2020/Solutions.sql
+++ 27 June 2020/Solutions.sql
@@ -179,5 +179,27 @@ WHERE j.Status LIKE 'Finished'
 GROUP BY j.JobId
 ORDER BY Total DESC, j.JobId
 
+-- 10. Missing Parts
+
+SELECT 
+p.PartId,
+p.Description, 
+pn.Quantity AS [Required],
+p.StockQty AS [In Stock],
+IIF(o.Delivered = 0, op.Quantity, 0) AS Ordered
+FROM Parts AS p
+LEFT JOIN PartsNeeded AS pn
+ON p.PartId = pn.PartId
+LEFT JOIN OrderParts AS op
+ON pn.PartId = op.PartId
+LEFT JOIN Jobs AS j
+ON pn.JobId = j.JobId
+LEFT JOIN Orders AS o
+ON j.JobId = o.JobId
+WHERE j.Status <> 'Finished' AND 
+p.StockQty + IIF(o.Delivered = 0, op.Quantity,0) < pn.Quantity 
+ORDER BY p.PartId
+
+
 
 
